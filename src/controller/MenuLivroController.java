2c858c1c03052f21b1144d8eac39b413603aac37
Arietis <54067766+Rigobertto@users.noreply.github.com>
2021-05-02 11:19:37 -0300

    adicionando controllers (apenas declaração)

---
+++ src/controller/MenuLivroController.java
@@ -0,0 +1,7 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import view.Tela;
+public class MenuLivroController{
+
+}
---
+++ src/controller/TelaLoginController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import view.Tela;
+
+public class TelaLoginController {
+	@FXML private TextField login;
+	@FXML private TextField senha;
+	@FXML private Label erroAut;
+
+	public void autenticar(ActionEvent event) {
+		
+	}
+}
